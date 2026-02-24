7d6c604c4b6db6b8316e0792923adc56dd9b77a9
Arthur Hua <arthur.hua.102@gmail.com>
2026-02-23 20:08:42 -0700

    going back to multiple.sh

--- transmitter.c
+++ transmitter.c
@@ -27,39 +27,59 @@ static void sleep_until(double target)
     ts.tv_nsec = (long)((remaining - ts.tv_sec) * 1e9);
     nanosleep(&ts, NULL);
 }
-
-static void hammer_memory(double until)
-{
-    pid_t pid = fork();
-    if (pid == 0) {
-        int devnull = open("/dev/null", O_WRONLY);
-        if (devnull == -1) {
-            perror("open");
+static void hammer_memory(double until) {
+    while (now() < until) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            int devnull = open("/dev/null", O_WRONLY);
+            if (devnull == -1) { perror("open"); _exit(1); }
+            dup2(devnull, STDOUT_FILENO);
+            dup2(devnull, STDERR_FILENO);
+            close(devnull);
+            execl("./simple_stream", "simple_stream", NULL);
+            perror("execl failed");
             _exit(1);
+        } else if (pid > 0) {
+            waitpid(pid, NULL, 0);  // wait for this run to finish, then loop
+        } else {
+            perror("fork failed");
+            break;
         }
-
-        dup2(devnull, STDOUT_FILENO);
-        dup2(devnull, STDERR_FILENO);
-
-        close(devnull);
-
-        execl("./simple_stream", "simple_stream", NULL);
-
-        perror("execl failed");
-        _exit(1);
     }
+// }
+// static void hammer_memory(double until)
+// {
+//     pid_t pid = fork();
+//     if (pid == 0) {
+//         int devnull = open("/dev/null", O_WRONLY);
+//         if (devnull == -1) {
+//             perror("open");
+//             _exit(1);
+//         }
 
-    else if (pid > 0) {
-        while (now() < until) {
-            usleep(1000);  
-        }
-        kill(pid, SIGKILL);
-        waitpid(pid, NULL, 0);
-    }
-    else {
-        perror("fork failed");
-    }
-}
+//         dup2(devnull, STDOUT_FILENO);
+//         dup2(devnull, STDERR_FILENO);
+
+//         close(devnull);
+//         while (1) {
+//             execl("./simple_stream", "simple_stream", NULL);
+//             // If exec fails:
+//             perror("execl failed");
+//             _exit(1);
+//         }
+//     }
+
+//     else if (pid > 0) {
+//         while (now() < until) {
+//             usleep(1000);  
+//         }
+//         kill(pid, SIGKILL);
+//         waitpid(pid, NULL, 0);
+//     }
+//     else {
+//         perror("fork failed");
+//     }
+// }
 
 // static void hammer_memory(double until)
 // {
